237527766899c653d3706da8057c1ce452881802
JwalithaKumar <154355829+JwalithaKumar@users.noreply.github.com>
2024-01-30 14:35:48 +0530

    Update signclassify.py

--- Sign-language/pages/signclassify.py
+++ Sign-language/pages/signclassify.py
@@ -4,6 +4,9 @@ import pandas as pd
 import random
 import streamlit.components.v1 as components
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.applications.mobilenet import preprocess_input
 
 st.set_page_config(
     page_title="SignLanguageDetection",
@@ -31,4 +34,10 @@ x = st.button("Predict")
 if x:
     with st.spinner("Thinking..."):
         model = load_model('./models/SignL.h5')
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = preprocess_input(img_array)
+        predictions = model.predict(img_array)
+        
+
 
