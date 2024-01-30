c28d876b00d11f3be8c2891411adca20b54c661b
JwalithaKumar <154355829+JwalithaKumar@users.noreply.github.com>
2024-01-30 14:31:23 +0530

    Update signclassify.py

--- Sign-language/pages/signclassify.py
+++ Sign-language/pages/signclassify.py
@@ -3,6 +3,7 @@ import imagerec
 import pandas as pd
 import random
 import streamlit.components.v1 as components
+from tensorflow.keras.models import load_model
 
 st.set_page_config(
     page_title="SignLanguageDetection",
@@ -29,27 +30,5 @@ if uploaded_file!=None:
 x = st.button("Predict")
 if x:
     with st.spinner("Thinking..."):
-        def imagerecognise(uploadedfile,modelpath,labelpath):
-            np.set_printoptions(suppress=True)
-            model = load_model(modelpath, compile=False)
-            class_names = open(labelpath, "r").readlines()
-            data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-            image = Image.open(uploadedfile).convert("RGB")
-            size = (224, 224)
-            image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
-            image_array = np.asarray(image)
-            normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
-            data[0] = normalized_image_array
-            prediction = model.predict(data)
-            index = np.argmax(prediction)
-            class_name = class_names[index]
-            confidence_score = prediction[0][index]
-            print("Class:", class_name[2:], end="")
-            print("Confidence Score:", confidence_score)
-            return(class_name[2:],confidence_score)
-        y,conf = imagerec.imagerecognise(uploaded_file,"models/SignL.h5","models/labels.txt")
-    
-    
-    x = random.randint(95,100)+ random.randint(0,99)*0.01
-  
-    st.warning("Accuracy : " + str(x) + " %")
+        model = load_model('./model/SignL.h5')
+
